8e407817a55fe97e96ae9fa6b42d723eb9654857
Edward <71886023+edward62740@users.noreply.github.com>
2022-12-10 18:18:01 +0800

    Added otmanager class

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from aiocoap import resource
 from server import AlarmResource
 import netifaces
 import subprocess
+from ot_manager import OtManager
 
 def main():
     # Resource tree creation
@@ -18,46 +19,28 @@ def main():
         ctr+=1
         if i['addr'].startswith('fd'):
             break
+    otM = OtManager(ipaddress.ip_address(addrs[netifaces.AF_INET6][ctr]['addr']))
+    otM.findChildIps()
+    ips = otM.getChildIps()
 
-    process = subprocess.Popen(['ot-ctl', 'childip'],
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-    ips = []
-    while True:
-        output = process.stdout.readline()
-        lines = output.strip().splitlines()
-        for line in lines:
-            try:
-                print(line[6:])
-                tmp = ipaddress.ip_address(line[6:])
-                ips.append(tmp)
-            except ValueError:
-                pass
-
-
-        print(ips)
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            break
-
-    asyncio.get_event_loop().run_until_complete(client(ips))
-    print("SERVER IP" + addrs[netifaces.AF_INET6][ctr]['addr'])
+    asyncio.get_event_loop().run_until_complete(client(otM))
     root = resource.Site()
     root.add_resource(['radar'], AlarmResource())
     asyncio.Task(aiocoap.Context.create_server_context(root, bind=(addrs[netifaces.AF_INET6][ctr]['addr'], 5683)))
     print("Server running")
     asyncio.get_event_loop().run_forever()
-    print("This is the end")
 
 
-async def client(ips: list[ipaddress.IPv6Address]):
+async def client(otm: OtManager):
     context = await Context.create_client_context()
     payload = b"000000"
-
-    for ip in ips:
-        print("Sent to %s" % ip)
-        request = Message(code=GET, payload=payload, uri="coap://[" + str(ip) + "]/permissions")
+    while True:
+        ip = otm.dequePendChildIps()
+        if ip != ipaddress.IPv6Address("::"):
+            print("Sent to %s" % ip)
+            request = Message(code=GET, payload=payload, uri="coap://[" + str(ip) + "]/permissions")
+        else:
+            break
 
         response = await context.request(request).response
         print('Result: %x\n%r' % (response.code, response.payload))
--- ot_manager.py
+++ ot_manager.py
@@ -6,23 +6,57 @@ from typing import Set, Type
 
 
 class OtManager:
-    child_ip6 = set[ipaddress.IPv6Address]
-    self_ip6 = ipaddress.IPv6Address
+    child_ip6 = set[IPv6Address]() # Child IPv6 sensitivity list
+    pend_queue_child_ips = set[IPv6Address]() # Queue for new children to be notified
+    self_ip6 = ipaddress.IPv6Address # CoAP server IPv6
 
     def __init__(self, self_ip: Type[IPv6Address]):
-        if Type[self_ip] is not IPv6Address:
-            raise ValueError
         self.self_ip6 = self_ip
+        print("Registered self ip as " + str(self.self_ip6))
 
-    def textToChildIps(self, text: str) -> None:
-        lines = text.strip().splitlines()
-        for line in lines:
+    def findChildIps(self) -> None:
+        process = subprocess.Popen(['ot-ctl', 'childip'],
+                                   stdout=subprocess.PIPE,
+                                   universal_newlines=True)
+        while True:
+            output = process.stdout.readlines()
+            lines = output
+            prefix = str(self.self_ip6)[:4]
+            for line in lines:
+                try:
+                    line = line.rstrip()
+                    if not line[6:].startswith(prefix):
+                        print(line[6:].strip()  + " does not match BR prefix " + prefix)
+                    else:
+                        tmp = ipaddress.ip_address(line[6:])
+                        if tmp not in self.child_ip6:
+                            self.pend_queue_child_ips.add(tmp)
+                            print(line[6:].strip()  + " added to child notif queue")
+                        self.child_ip6.add(tmp)
+                        print(line[6:].strip()  + " updated in child sensitivity list")
+                except ValueError:
+                    pass
+
+            last = str(process.poll())
             try:
-                print(line[6:])
-                tmp = ipaddress.ip_address(line[6:])
+                last = last.rstrip()
+                if last not in self.child_ip6:
+                    self.pend_queue_child_ips.add(last)
+                    print(last[6:].strip() + " added to child notif queue")
+                print(last[6:].strip()  + " added to child sensitivity list")
+                tmp = ipaddress.ip_address(last[6:])
                 self.child_ip6.add(tmp)
             except ValueError:
                 pass
+            if last is not None:
+                break
 
-    def getChildIps(self) -> Type[set[IPv6Address]]:
+    def getChildIps(self) -> set[IPv6Address]:
         return self.child_ip6
+
+    def dequePendChildIps(self) -> IPv6Address:
+        try:
+            ret = self.pend_queue_child_ips.pop()
+            return ret
+        except KeyError:
+            return ipaddress.ip_address("::")
